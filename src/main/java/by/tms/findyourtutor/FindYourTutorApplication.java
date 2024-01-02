db073d7531e88a634ad39e12afc27e8833d5493b
Irina-Kr <133982815+Irina-Kr@users.noreply.github.com>
2024-01-02 19:23:30 +0300

    findYourTutorFirstCommit

---
+++ src/main/java/by/tms/findyourtutor/FindYourTutorApplication.java
@@ -0,0 +1,13 @@
+package by.tms.findyourtutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindYourTutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindYourTutorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/tms/findyourtutor/FindYourTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.findyourtutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindYourTutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
